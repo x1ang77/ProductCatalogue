f99b36ae784f985ccebc45c41f99edd9bc039a9a
nathalie7980 <londonhippos@gmail.com>
2023-02-28 13:39:41 +0800

    third commit

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/products", require("./api/products"));
-// app.use("/leave", require("./api/leave"));
 
 app.listen(5000, () => console.log("App is flying on PORT 5000"));
 
