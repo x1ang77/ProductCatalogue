f2bf595e16126b0fa64fef8ddc4aecba88c75d33
Xiang Ze <xchuay@gmail.com>
2023-03-01 10:33:36 +0800

    first commit

--- api/products.js
+++ api/products.js
@@ -5,48 +5,84 @@ const Product = require("../models/Product");
 
 //get products
 router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    return res.json(products);
-  } catch {
-    return res.status(500).json({ message: "Server Error" });
-  }
+    try {
+        const products = await Product.find();
+        return res.json(products);
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id);
+        if (!productFound)
+            return res.status(404).json({ message: "Product not found" });
+        else return res.json(productFound);
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id);
+        if (!productFound)
+            return res.status(404).json({ message: "Product not found" });
+        await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json({
+            productFound,
+            message: "Product updated successfully",
+        });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id, req.body);
+        return res.json({
+            message: "Product updated successfully",
+        });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const {
-      title,
-      brand,
-      category,
-      description,
-      price,
-      discountPercentage,
-      rating,
-      stock,
-      thumbnail,
-    } = req.body;
+    try {
+        const {
+            title,
+            brand,
+            category,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            thumbnail,
+        } = req.body;
 
-    const product = new Product({
-      title,
-      brand,
-      category,
-      description,
-      price,
-      discountPercentage,
-      rating,
-      stock,
-      thumbnail,
-    });
+        const product = new Product({
+            title,
+            brand,
+            category,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            thumbnail,
+        });
 
-  
-    await product.save();
-    return res
-      .status(200)
-      .json({ product, msg: "New product added successfully" });
-  } catch {
-    return res.status(500).json({ message: "Server Error" });
-  }
+        await product.save();
+        return res
+            .status(200)
+            .json({ product, msg: "New product added successfully" });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 module.exports = router;
--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  title: { type: String },
-  brand: { type: String },
-  category: { type: String },
-  description: { type: String },
-  price: { type: Number },
-  discountPercentage: { type: Number },
-  rating: { type: Number },
-  stock: { type: Number },
-  thumbnail: { type: String },
+    title: { type: String },
+    brand: { type: String },
+    category: { type: String },
+    description: { type: String },
+    price: { type: Number },
+    discountPercentage: { type: Number },
+    rating: { type: Number },
+    stock: { type: Number },
+    thumbnail: { type: String },
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
